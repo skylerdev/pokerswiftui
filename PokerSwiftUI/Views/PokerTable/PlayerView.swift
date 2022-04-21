709bf06611870a68c6fdf033d5535d0c8c59cb7e
Skyler <skylerdev@icloud.com>
2022-04-21 12:54:25 -0700

    upgrade PlayerView appearance

--- PokerSwiftUI/Views/PokerTable/PlayerView.swift
+++ PokerSwiftUI/Views/PokerTable/PlayerView.swift
@@ -12,17 +12,63 @@ struct PlayerView: View {
     
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 3) {
-            Text(player.name)
-            if player.bet != 0 {
-                Text("Bet: \(player.bet)")
+        
+        ZStack {
+            
+                //MARK: - Betting
+                VStack {
+                    HStack {
+                        Text("Bets:")
+                        Text("\(player.bet)").bold()
+                    }
+                    .padding(.top, 10)
+                }
+                .padding(10)
+                .background(.ultraThinMaterial)
+                .cornerRadius(10)
+                .offset(y: player.bet != 0 ? 50 : 0)
+                .animation(.spring(), value: player.bet)
+                
+                
+                
+            
+            
+            VStack {
+                VStack(alignment: .leading, spacing: 3) {
+                    Text(player.name).bold()
+                   
+                        HStack(){
+                        Image("PokerChip")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 25, height: 25)
+                            Text("\(player.chips)")
+                                                
+                        }
+                    
+                }
+                .padding()
+                .background(.ultraThickMaterial)
+            .cornerRadius(10)
+                
+              
+                    
+                
+                
             }
         }
+        
+    
+        
     }
 }
 
 struct PlayerView_Previews: PreviewProvider {
     static var previews: some View {
-        PlayerView(player: Player(id: "jfjf", name: "bungler"))
+        
+        PlayerView(player: Player(id: "jfjf", name: "Dunco El Guapo", bet: 999999))
+            .frame(width: 300, height: 300)
+            .background(.cyan)
+        
     }
 }
