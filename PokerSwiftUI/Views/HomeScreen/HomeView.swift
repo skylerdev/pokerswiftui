1771903eb84a0d4e956eda7f42c445499f359d08
andrew-bdev <andrew-black@outlook.com>
2022-11-09 13:13:21 -1000

    HomeView Changes

--- PokerSwiftUI/Views/HomeScreen/HomeView.swift
+++ PokerSwiftUI/Views/HomeScreen/HomeView.swift
@@ -12,76 +12,113 @@ struct HomeView: View {
     @EnvironmentObject var tableModel: TableModel
     @State private var code: String = ""
     @State private var name: String = ""
-
+    
     @State private var nameValid = false
+    @State private var hostToggled = false
+    @State var isAnimating = false // <1>
     
     @State private var invalidFeedback: String = ""
     
     var body: some View {
         NavigationView {
-            VStack(alignment: .center) {
-                //The Title
-                Text("PokerChips")
-                    .bold()
-                    .font(.largeTitle)
+            ZStack {
+                Image("placeholdertable")
+                    .resizable()
                 
                 
-                //The Name Field
-                TextField("Your Silly Little Name", text: $name, onEditingChanged: { isEditing in
-                    if !isEditing {
-                        nameValid = nameValidator()
-                    }})
-                    //.textInputAutocapitalization(.never)
-                    .disableAutocorrection(true)
-                    //.border(.secondary)
-                    .limitInputLength(value: $name, length: 16)
                 
-                //The Room Code Field
-                TextField("Room Code", text: $code, onEditingChanged: { isEditing in
-                    if !isEditing {
-                        codeValidator()
-                    }})
-                    //.textInputAutocapitalization(.never)
-                    .disableAutocorrection(true)
-                    //.border(.secondary)
-                    .limitInputLength(value: $code, length: 4)
+                VStack(alignment: .center) {
+                    //The Title
+                    Text("PokerChips")
+                        .bold()
+                        .font(.largeTitle)
                     
-                
-                //Lets you join or host room
-//                NavigationLink(isActive: $tableModel.hosting) {
-//                    TableHost()
-//                } label: { EmptyView() }
-//
-//                NavigationLink(isActive: $tableModel.joining) {
-//                    TableHost()
-//                } label: { EmptyView() }
-                
-                
-                //Buttons
-                HStack(alignment: .center, spacing: 10) {
-                    HostButton(pressed: hostPressed, valid: nameValid, text: "Host")
-                    HostButton(pressed: joinPressed, valid: nameValid && tableModel.exists, text: "Join")
-                }
+                    //The Name Field
+                    TextField("Your Silly Little Name", text: $name, onEditingChanged: { isEditing in
+                        if !isEditing {
+                            nameValid = nameValidator()
+                        }})
+                    .offset(x:10)
+                    .frame(height: 50)
+                    .background(.thickMaterial)
+                    .cornerRadius(10)
+                    .padding(.horizontal)
+                    .textInputAutocapitalization(.never)
+                    .disableAutocorrection(true)
+                    .zIndex(1)
+                    
+                    
+                    HStack {
+                    //The Room Code Field
+                        TextField(hostToggled ? "" : "Room Code", text: $code, onEditingChanged: { isEditing in
+                        if !isEditing {
+                            codeValidator(input: code)
+                        }})
+                    .offset(x: 10)
+                    .foregroundColor(hostToggled ? .clear : .black)
+                    .textInputAutocapitalization(.never)
+                    .disableAutocorrection(true)
+                    .disabled(hostToggled)
+                    
+                        Divider()
+                            .offset(x : -20)
+                        
+                    Toggle(isOn: $hostToggled) {
+                        Text("Hosting?")
+                    }
+                    .offset(x: -10)
+                    }
+                    .frame(height: 50)
+                    .background(.thickMaterial)
+                    .cornerRadius(10)
+                    .padding(.horizontal)
+                    
+                    
+                    HStack(alignment: .center) {
+                        HostButton(pressed: hostPressed, valid: nameValid, text: "Host")
+                            .padding()
+                        HostButton(pressed: joinPressed, valid: nameValid && tableModel.exists, text: "Join")
+                            .padding()
+                    }
+                    
+                    //.background(.blue)
+                    
+                    //Lets you join or host room
+                    NavigationLink(isActive: $tableModel.hosting) {
+                        TableHost()
+                    } label: { EmptyView() }
+                    
+                    NavigationLink(isActive: $tableModel.joining) {
+                        TableHost()
+                    } label: { EmptyView() }
+                    
+                    
+                    //Buttons
+                    
                     
                     //TODO: Use an internal codeValid state in combination with .exists and nameValid?
                     //That way we can wait for .exists and even maybe add a loading indicator
-                
-                //Invalid feedback
-                Text(tableModel.exists ? "" : invalidFeedback)
-                    .foregroundColor(tableModel.exists ? .green : .red)
-                    .animation(.spring().delay(0.2), value: tableModel.exists)
-                    .frame(width: 200)
-                    .animation(.spring().speed(10), value: invalidFeedback)
-              
-                
-                
-                Spacer()
-            }.padding()
+                    
+                    //Invalid feedback
+                    Text(tableModel.exists ? "" : invalidFeedback)
+                        .foregroundColor(tableModel.exists ? .green : .red)
+                        .animation(.spring().delay(0.2), value: tableModel.exists)
+                        .frame(width: 200)
+                        .animation(.spring().speed(10), value: invalidFeedback)
+                    
+                    
+                    
+                    Spacer()
+                }
+            }
         }
     }
     
     func nameValidator() -> Bool {
         if(name.isEmpty){
+            return false
+        }
+        if(name.count > 15){
             return false
         }
         //TODO: alphanumeric check here please
@@ -91,8 +128,7 @@ struct HomeView: View {
     }
     
     func joinPressed() {
-        print("joinPressed: setting model to entered code \(code)")
-        tableModel.tableId = code
+        print("tried to join")
         if(tableModel.exists){
             invalidFeedback = "Trying To Join..."
             print("joining game \(tableModel.tableId)")
@@ -109,22 +145,22 @@ struct HomeView: View {
         invalidFeedback = "Trying To Host..."
         tableModel.dataInitCallback = {
             tableModel.hosting = true
-            
         }
         tableModel.hostGame()
-       
+        
     }
     
-    func codeValidator() {
+    func codeValidator(input: String) {
+        tableModel.tableId = input.lowercased()
         tableModel.exists = false
         if(code.isEmpty){
-            invalidFeedback = "Invalid"
+            invalidFeedback = "Empty"
         }else if(code.count != 4){
-            invalidFeedback = "Invalid"
+            invalidFeedback = "Not 4 chars"
         }else{
             invalidFeedback = "Checking..."
             //code is valid, actually check db now
-            tableModel.gameExists(gameID: code) {
+            tableModel.gameExists(gameID: code){
                 invalidFeedback = tableModel.exists ? "Exists" : "Game does not exist"
             }
             
@@ -137,24 +173,6 @@ struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
             .environmentObject(TableModel())
-    }
-}
-
-
-struct TextFieldLimitModifer: ViewModifier {
-    @Binding var value: String
-    var length: Int
-
-    func body(content: Content) -> some View {
-        content
-            .onReceive(value.publisher.collect()) {
-                value = String($0.prefix(length))
-            }
-    }
-}
-
-extension View {
-    func limitInputLength(value: Binding<String>, length: Int) -> some View {
-        self.modifier(TextFieldLimitModifer(value: value, length: length))
+            .previewInterfaceOrientation(.portrait)
     }
 }
