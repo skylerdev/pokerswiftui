1bfba5e29aa1f4fcddbfa602b318eefab81c6a2a
Skyler <skylerdev@icloud.com>
2022-11-19 14:48:59 -0800

    visual progress

--- PokerSwiftUI/Views/HomeScreen/ValidView.swift
+++ PokerSwiftUI/Views/HomeScreen/ValidView.swift
@@ -9,16 +9,106 @@ import SwiftUI
 
 struct ValidView: View {
     
-    @State private var valid = false
-    @State private var checking = false
+    private var threeDimensional = false
     
+    @State public var valid = false
+    @State public var checking = false
+    
+    @State public var visible = true
+    
+    @State private var displaying = 0
+
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if(checking){
+            if(threeDimensional){
+                hyperCheckingText()
+            }else{
+                checkingText()
+                    .opacity(visible ? 1 : 0)
+            }
+        }else{
+            Text(valid ? "Valid" : "Invalid")
+                .padding()
+                .overlay(
+                    Capsule()
+                    .stroke(style: StrokeStyle(lineWidth: 2, dash: [5, 10]))
+                    .onAppear {
+                        withAnimation(.linear) {
+                            displaying += 1
+                        }
+                    }
+                    
+                )
+        }
+       
     }
 }
+
+struct hyperCheckingText : View {
+    
+    @State private var degrees: CGFloat = 360
+    
+    var body: some View {
+        
+        Text("Checking...")
+            .foregroundColor(.black)
+            .padding()
+            //First Circle
+            .overlay(
+                Circle()
+                .scale(2)
+                .stroke(Color.green, style: StrokeStyle(lineWidth: 3, dash: [1]))
+                .rotation3DEffect(.degrees(degrees), axis: (x: 1, y: 1, z: 0))
+                .animation(.linear(duration: 3)
+                    .repeatForever(autoreverses: false),
+                           value: degrees)
+                    .onAppear {
+                        degrees = 1
+                    }
+            )
+            //Second Circle
+            .overlay( Circle()
+                .scale(2)
+                .stroke(Color.green, style: StrokeStyle(lineWidth: 3, dash: [1]))
+                .rotation3DEffect(.degrees(degrees), axis: (x: 1, y: -1, z: 0))
+                .animation(.linear(duration: 3)
+                    .repeatForever(autoreverses: false),
+                           value: degrees)
+                    .onAppear {
+                        degrees = 1
+                    }
+            )
+    }
+}
+
+
+struct checkingText : View {
+    
+    @State private var phase = 0.0
+
+    var body: some View {
+        
+        Text("Checking...")
+            .padding()
+            .overlay(
+                Capsule()
+                .scale(1)
+                .stroke(style: StrokeStyle(lineWidth: 2, dash: [50, 5, 20, 5], dashPhase: phase))
+                .onAppear {
+                    withAnimation(.linear.repeatForever(autoreverses: false)) {
+                        phase += 90
+                    }
+                }
+                
+            )
+    }
+}
+
 
 struct ValidView_Previews: PreviewProvider {
     static var previews: some View {
         ValidView()
+        checkingText()
     }
 }
