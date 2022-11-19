a399693d3df410c54b476c4543db0839b274e1c3
Skyler <skylerdev@icloud.com>
2022-11-19 13:10:15 -0800

    add ValidView, a fancy code validator

---
+++ PokerSwiftUI/Views/HomeScreen/ValidView.swift
@@ -0,0 +1,24 @@
+//
+//  ValidView.swift
+//  PokerSwiftUI
+//
+//  Created by Skyler Devlaming on 2022-11-17.
+//
+
+import SwiftUI
+
+struct ValidView: View {
+    
+    @State private var valid = false
+    @State private var checking = false
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ValidView_Previews: PreviewProvider {
+    static var previews: some View {
+        ValidView()
+    }
+}
